ce0ca94b06b3c734a4764de0f7130a6069ff6c3a
Clifford Grimmell and Shane Stafford <clifford@>
2017-02-13 16:59:55 -0800

    add function line to script

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
-var userSent = function (sentence) {
-  alert("Your sentence ciphered is: " + sentence);
-}
 var UserInput = prompt("What sentence would you like ciphered? ");
 var change1 = UserInput.charAt(0);
 var change2 = UserInput.charAt(UserInput.length -1);
 var change3 = change1 + change2
-var change3 = change3.toUpperCase();
+change3 = change3.toUpperCase();
 var reversestring = function(str) {
   var split = str.split("");
   var reverse = split.reverse();
@@ -13,5 +10,8 @@ var reversestring = function(str) {
   return join;
   alert(join)
 }
-var reversed = reversestring(change3)
-userSent(reversed)
+var userSent = function (sentence) {
+  alert("Your sentence ciphered is: " + sentence);
+}
+var reversed = reversestring(change3);
+var revstring = userSent(reversed);
